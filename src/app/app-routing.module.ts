467dc2f9839043be357217a507ad8a08451712a0
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2021-12-24 14:33:07 +0530

    routelinks added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AndroidComponent } from './it/android/android.component';
+import { AngularComponent } from './it/angular/angular.component';
+import { IotComponent } from './it/iot/iot.component';
+import { JavaComponent } from './it/java/java.component';
+import { PythonComponent } from './it/python/python.component';
+import { ReactComponent } from './it/react/react.component';
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch:'full'},
   {path: 'home', component: HomeComponent },
-  {path:'android', component:AndroidComponent }
+  {path:'android', component:AndroidComponent },
+  {path:'angular', component:AngularComponent },
+  {path:'iot', component:IotComponent },
+  {path:'java', component:JavaComponent },
+  {path:'python', component:PythonComponent },
+  {path:'react', component:ReactComponent }
 
 ];
 
